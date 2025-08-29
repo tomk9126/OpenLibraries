2f9350ef32b246097d70f84665ca8d44aacbab1f
tomk9126 <132254596+tomk9126@users.noreply.github.com>
2025-08-29 20:07:04 +1000

    Added more attributes to classes, added getters and setters to rewrite private object data.

--- Author.java
+++ Author.java
@@ -1,11 +1,24 @@
 public class Author {
+    
+    private long id;
     private String firstName;
     private String lastName;
 
+    //Constructor
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    //Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getBio() { return bio; }
+    public void setBio(String bio) { this.bio = bio; }
 
     @Override
     public String toString() {
--- Book.java
+++ Book.java
@@ -1,9 +1,33 @@
 public class Book {
-    private String name;
-
-    public Book(String bookName) {
-        this.name = bookName;
+    
+    private long id;
+    private String isbn;
+    private String title;
+    private String publisher;
+    private int year;
+    
+    //Constructor Class
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
     }
+
+    //Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    public Integer getYear() { return year; }
+    public void setYear(Integer year) { this.year = year; }
+
 
     @Override
     public String toString() {
